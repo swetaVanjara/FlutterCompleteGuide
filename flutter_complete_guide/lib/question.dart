08920c423d62448e35f3c737511a1e296d3fa211
“Sweta <vanjarasweta@gmail.com>
2021-11-08 00:39:37 +0530

    styling question widget

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -8,6 +8,17 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      //container as much size as much width so basically width of the device so can centering text otherwise it'll take text size.
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
