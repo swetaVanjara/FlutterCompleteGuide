1158d6de8f005dcbca01e5d37deb3f95dc3ad746
“Sweta <vanjarasweta@gmail.com>
2021-11-07 23:30:27 +0530

    added layout widget- column

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -11,14 +11,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
           backgroundColor: Colors.deepOrange,
         ),
-        body: Center(
-          child: Text("Hello Shweta"),
+        body: Column(
+          children: [
+            Text("The Question!"),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Answer 2"),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Answer 3"),
+            ),
+          ],
         ),
       ),
     );
